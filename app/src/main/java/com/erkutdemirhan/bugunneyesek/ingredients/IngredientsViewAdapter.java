bbe220a0db2d91808b8ac1a5cee17ccf06e46630
Erkut Demirhan <erkutdemirhan@gmail.com>
2015-05-16 17:42:24 +0300

    Ingredients list was updated so that each item set as a cardview

---
+++ app/src/main/java/com/erkutdemirhan/bugunneyesek/ingredients/IngredientsViewAdapter.java
@@ -0,0 +1,87 @@
+package com.erkutdemirhan.bugunneyesek.ingredients;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.erkutdemirhan.bugunneyesek.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Erkut Demirhan on 13.05.2015.
+ */
+public class IngredientsViewAdapter extends RecyclerView.Adapter<IngredientsViewAdapter.ViewHolder> {
+
+    private List<String> mList;
+    private RecyclerView mRecyclerView;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTextView;
+        private ImageView mImageView;
+        private View mView;
+
+        public ViewHolder(View v) {
+            super(v);
+            mView = v;
+            mTextView = (TextView) v.findViewById(R.id.ingredient_name);
+            mImageView = (ImageView) v.findViewById(R.id.remove_ingredient_icon);
+            mImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = mRecyclerView.getChildPosition(mView);
+                    removeAt(position);
+                }
+            });
+        }
+
+        public void setTextView(String text) {
+            mTextView.setText(text);
+        }
+    }
+
+    public IngredientsViewAdapter(RecyclerView recyclerView) {
+        mRecyclerView = recyclerView;
+        mList = new ArrayList<>();
+    }
+
+    @Override
+    public IngredientsViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.ingredients_list_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(IngredientsViewAdapter.ViewHolder holder, int position) {
+        holder.setTextView(mList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public void removeAt(int position) {
+        mList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void addItem(String item) {
+        mList.add(item);
+        notifyItemInserted(mList.size()-1);
+    }
+
+    public boolean isItemAddedBefore(String item) {
+        for(String ingredient:mList) {
+            if(ingredient.equalsIgnoreCase(item)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/erkutdemirhan/bugunneyesek/ingredients/SelectIngredients.java
+++ app/src/main/java/com/erkutdemirhan/bugunneyesek/ingredients/SelectIngredients.java
@@ -4,20 +4,86 @@ package com.erkutdemirhan.bugunneyesek.ingredients;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.erkutdemirhan.bugunneyesek.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Erkut Demirhan on 12.05.2015.
  */
 public class SelectIngredients extends Fragment {
 
+    private List<String> mIngredients;
+    private AutoCompleteTextView mAutoCompleteTextView;
+    private ArrayAdapter<String> mAutoCompleteAdapter;
+
+    private RecyclerView mRecyclerView;
+    private IngredientsViewAdapter mIngredientsViewAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private Button mAddIngredientButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.select_ingredients, container, false);
-        return v;
+        View rootView =inflater.inflate(R.layout.select_ingredients, container, false);
+
+        populateIngredientsList();
+
+        mAutoCompleteTextView = (AutoCompleteTextView) rootView.findViewById(R.id.select_ingredients_autocomplete);
+        mAutoCompleteAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line, mIngredients);
+        mAutoCompleteTextView.setAdapter(mAutoCompleteAdapter);
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.select_ingredients_recyclerview);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mIngredientsViewAdapter = new IngredientsViewAdapter(mRecyclerView);
+        mRecyclerView.setAdapter(mIngredientsViewAdapter);
+
+        mAddIngredientButton = (Button) rootView.findViewById(R.id.select_ingredients_button);
+        mAddIngredientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addIngredient();
+            }
+        });
+
+        return rootView;
+    }
+
+    private void populateIngredientsList() {
+        mIngredients = new ArrayList<>();
+        mIngredients.add("Domates");
+        mIngredients.add("Patates");
+        mIngredients.add("Limon");
+        mIngredients.add("Ayçiçek Yağı");
+        mIngredients.add("Tuz");
+        mIngredients.add("Karabiber");
+        mIngredients.add("Patlıcan");
+        mIngredients.add("Havuç");
+        mIngredients.add("Ispanak");
+        mIngredients.add("Salatalık");
+    }
+
+    private void addIngredient() {
+        String ingredient = mAutoCompleteTextView.getText().toString();
+        if(!ingredient.equals("")) {
+            if(mIngredientsViewAdapter.isItemAddedBefore(ingredient)) {
+                Toast.makeText(this.getActivity(), "Bu malzemeyi listeye daha önceden eklediniz!", Toast.LENGTH_SHORT).show();
+            } else {
+                mIngredientsViewAdapter.addItem(ingredient);
+            }
+        }
     }
 }
